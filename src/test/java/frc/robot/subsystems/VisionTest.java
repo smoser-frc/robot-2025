e6b6735e9562a6f5f00a34a9f925775503943dba
Scott Moser <smoser@brickies.net>
2025-02-20 18:37:48 -0500

    Add a simple test as an example

---
+++ src/test/java/frc/robot/subsystems/VisionTest.java
@@ -0,0 +1,13 @@
+package frc.robot.commands;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+
+public class VisionTest {
+
+  @Test
+  public void test1() {
+    Assertions.assertEquals("foo", "foo");
+  }
+
+}
